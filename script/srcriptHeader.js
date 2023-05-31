03bfab1a6f0a40470cf4b4afc6b3076a96fb8768
mgblanco10 <mgblanco1001@gmail.com>
2023-05-31 12:38:32 +0200

    wip of css and refactor code

--- script/srcriptHeader.js
+++ script/srcriptHeader.js
@@ -3,20 +3,20 @@ const buttonMenu = document.querySelector("#button-menu-open");
 const buttonClose = document.querySelector("#button-menu-close");
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
     menu.style.top = "-400px";
     buttonClose.style.display = "none";
     buttonMenu.style.display = "flex";
-  } else {
-    menu.classList.add("showMenu");
+  } else{
+    menu.classList.add("show");
     menu.style.top = "15%";
     buttonClose.style.display = "flex";
     buttonMenu.style.display = "none";
   }
 }
 
-buttonClose.style.display = "none"; // Ocultar el botón de cierre al inicio
+buttonClose.style.display = "none"; 
 
 buttonMenu.addEventListener("click", toggleMenu);
 buttonClose.addEventListener("click", toggleMenu);
