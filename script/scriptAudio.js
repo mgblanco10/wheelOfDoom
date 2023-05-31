1265fae2d44f64772a3ffcc336e53d14a24b11de
mgblanco10 <mgblanco1001@gmail.com>
2023-05-31 08:28:22 +0200

    wip sound

--- script/scriptAudio.js
+++ script/scriptAudio.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var btnStart = document.querySelector('.btnStart');
-    var audioPlayer = document.getElementById('audioPlayer');
-  
-    btnStart.addEventListener('click', function () {
-      audioPlayer.play();
-    });
+  var btnStart = document.querySelector('.btnStart');
+  var audioPlayer = document.getElementById('audioPlayer');
+
+  btnStart.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    audioPlayer.currentTime = 0; 
+    audioPlayer.play(); 
+
+    setTimeout(function () {
+      window.location.href = "/page/index.html"; 
+    }, 1000); 
   });
+});
