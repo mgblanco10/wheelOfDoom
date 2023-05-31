b87b62329111b4e093ccc322c2aaa4b3e0baf03d
mgblanco10 <mgblanco1001@gmail.com>
2023-05-31 17:21:20 +0200

    wip proyect wheel of dom

--- script/scriptChangePage.js
+++ script/scriptChangePage.js
@@ -1,8 +1,8 @@
 function playGame() {
-  var inputName = document.getElementById('inputName');
-  var buttonAgree = document.getElementsByClassName('btnAgree')[0];
-  var buttonPlay = document.getElementsByClassName('buttonPlay')[0];
-  var starContainer = document.getElementsByClassName('star-container')[0];
+  let inputName = document.getElementById('inputName');
+  let buttonAgree = document.getElementsByClassName('btnAgree')[0];
+  let buttonPlay = document.getElementsByClassName('buttonPlay')[0];
+  let starContainer = document.getElementsByClassName('star-container')[0];
   var telescope = document.querySelector('.telescope');
   var stars = document.querySelectorAll('.star');
   var namesList = document.getElementById('namesList');
@@ -13,7 +13,7 @@ function playGame() {
   telescope.style.display = 'none';
   namesList.style.display = 'none';
 
-  for (var i = 0; i < stars.length; i++) {
+  for (let i = 0; i < stars.length; i++) {
     stars[i].style.display = 'none';
   }
 
@@ -26,28 +26,30 @@ function playGame() {
 
   var starWidth = 30;
   var starHeight = 30;
-  var spaceBetweenStars = 10;
+  var spaceBetweenStars = 15;
+
 
   var containerWidth = starContainer.offsetWidth;
+  var starsPerRow = Math.max(Math.floor(containerWidth / (starWidth + spaceBetweenStars)), 5);
 
-  var starsPerRow = Math.floor(containerWidth / (starWidth + spaceBetweenStars));
-
-  for (var i = 0; i < numStars; i++) {
-    var star = starList.cloneNode(true);
-    star.id = 'star-' + i;
-
-    var row = Math.floor(i / starsPerRow);
-    var col = i % starsPerRow;
-
-    var starX = col * (starWidth + spaceBetweenStars);
-    var starY = row * (starHeight + spaceBetweenStars);
-
-    star.style.left = starX + 'px';
-    star.style.top = starY + 'px';
-
-    star.className = 'star';
-
-    starContainer.appendChild(star);
+  for (let i = 0; i < numStars; i++) {
+    const row = Math.floor(i/ starsPerRow);
+    const col = i % starsPerRow;
+  
+    const starX = col * (starWidth + spaceBetweenStars);
+    const starY = row * (starHeight + spaceBetweenStars);
+  
+    const starHTML = `
+      <img
+        id="star-${i}"
+        class="star"
+        style="left: ${starX}px; top: ${starY}px;"
+        src="/images/starYellow.png"
+        alt="Imagen de estrella sobre la imagen de fondo que parpadea y que representa la cantidad de la lista"
+      >
+    `;
+  
+    starContainer.insertAdjacentHTML('beforeend', starHTML);
   }
 
   var newStar = document.createElement('img');
@@ -69,11 +71,7 @@ function playGame() {
   numbers1.className = 'numbers';
   container1.appendChild(numbers1);
 
-  var button1 = document.createElement('button');
-  button1.className = 'btnGame';
-  button1.textContent = 'INICIAR';
-  container1.appendChild(button1);
-
+  
   var container2 = document.createElement('div');
   container2.className = 'containerInfo2';
   starContainer.parentNode.insertBefore(container2, starContainer.nextSibling);
@@ -82,10 +80,15 @@ function playGame() {
   title2.className = 'titleStatus';
   title2.textContent = 'Estrellas latentes';
   container2.appendChild(title2);
-
+  
   var numbers2 = document.createElement('div');
   numbers2.className = 'numbers';
   container2.appendChild(numbers2);
+
+  var button1 = document.createElement('button');
+  button1.className = 'btnGame';
+  button1.textContent = 'INICIAR';
+  container1.appendChild(button1);
 
   function counterDraw() {
     numbers2.textContent = players.length;
@@ -94,7 +97,7 @@ function playGame() {
   counterDraw();
 
   function playerRemoved() {
-    if (players.length > 0) {
+    if (players.length >= 0) {
       var starId = 'star-' + playersRemoved.length;
       var starToRemove = document.getElementById(starId);
       if (starToRemove) {
